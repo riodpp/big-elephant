387277c3aea2e1e3de1c019a1b78da8ccbd8f4f9
Rio Dwi Putra <rio.dwi@wartek.belajar.id>
2025-01-19 09:38:47 +0700

    Add cleaned logic using merge

--- dags/dag_postgres.py
+++ dags/dag_postgres.py
@@ -1,5 +1,6 @@
 from airflow import DAG
 from airflow.models import Variable as var
+from airflow.operators.dummy import DummyOperator
 from airflow.operators.python import BranchPythonOperator
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.providers.google.cloud.operators.bigquery import BigQueryInsertJobOperator
@@ -30,8 +31,9 @@ def check_table():
 with DAG(
     dag_id="test_postgres_connection",
     start_date=datetime(2019, 1, 1),
-    schedule_interval=None,
+    schedule_interval='0 1 * * *',
     catchup=False,
+    max_active_runs=1
 ) as dag:
 
     postgres_to_gcs = PostgresToGCSOperator(
@@ -74,6 +76,8 @@ with DAG(
         python_callable=check_table,
     )
 
+    skip_create_table = DummyOperator(task_id="skip_create_table")
+
     create_table = BigQueryCreateEmptyTableOperator(
         task_id="create_table",
         dataset_id="airflow_project",
@@ -83,6 +87,7 @@ with DAG(
             {'name': 'subscriber_type', 'type': 'STRING', 'mode': 'NULLABLE'},
             {'name': 'bike_id', 'type': 'STRING', 'mode': 'NULLABLE'},
             {'name': 'bike_type', 'type': 'STRING', 'mode': 'NULLABLE'},
+            {'name': 'start_time', 'type': 'TIMESTAMP', 'mode': 'NULLABLE'},
             {'name': 'start_date', 'type': 'DATE', 'mode': 'NULLABLE'},
             {'name': 'hour_of_day', 'type': 'INTEGER', 'mode': 'NULLABLE'},
             {'name': 'start_station_name', 'type': 'STRING', 'mode': 'NULLABLE'},
@@ -90,6 +95,79 @@ with DAG(
             {'name': 'duration_minutes', 'type': 'INTEGER', 'mode': 'NULLABLE'},
             {'name': 'duration_hours', 'type': 'INTEGER', 'mode': 'NULLABLE'},
         ],
+    )
+
+    clean_data_query = """
+        MERGE INTO `{project_id}.{dataset_id}.bike_trips_cleaned` T
+        USING (
+            SELECT 
+                trip_id,
+                subscriber_type,
+                bike_id,
+                bike_type,
+                start_time,
+                DATE(start_time) AS start_date,
+                EXTRACT(HOUR FROM start_time) AS hour_of_day,
+                start_station_name,
+                end_station_name,
+                duration_minutes,
+                CAST((duration_minutes / 60) AS INTEGER) AS duration_hours
+            FROM `{project_id}.{dataset_id}.bike_trips`
+            WHERE DATE(start_time) = DATE('{{{{ ds }}}}')
+        ) S
+        ON T.trip_id = S.trip_id
+        WHEN MATCHED THEN
+            UPDATE SET
+                subscriber_type = S.subscriber_type,
+                bike_id = S.bike_id,
+                bike_type = S.bike_type,
+                start_time = S.start_time,
+                start_date = S.start_date,
+                hour_of_day = S.hour_of_day,
+                start_station_name = S.start_station_name,
+                end_station_name = S.end_station_name,
+                duration_minutes = S.duration_minutes,
+                duration_hours = S.duration_hours
+        WHEN NOT MATCHED THEN
+            INSERT (
+                trip_id,
+                subscriber_type,
+                bike_id,
+                bike_type,
+                start_time,
+                start_date,
+                hour_of_day,
+                start_station_name,
+                end_station_name,
+                duration_minutes,
+                duration_hours
+            )
+            VALUES (
+                S.trip_id,
+                S.subscriber_type,
+                S.bike_id,
+                S.bike_type,
+                S.start_time,
+                S.start_date,
+                S.hour_of_day,
+                S.start_station_name,
+                S.end_station_name,
+                S.duration_minutes,
+                S.duration_hours
+            )
+    """.format(project_id=project_id, dataset_id=dataset_id)
+
+    clean_data = BigQueryInsertJobOperator(
+        task_id="clean_data",
+        configuration={
+            "query": {
+                "query": clean_data_query,
+                "useLegacySql": False,
+                "priority": "BATCH",
+            }
+        },
+        location='US',
+        trigger_rule='none_failed',
     )
 
     query = f"""
@@ -102,7 +180,7 @@ with DAG(
                 `airflow_project.bike_trips` t
             JOIN 
                 `airflow_project.bike_stations` s ON t.start_station_id = s.station_id
-                
+
             GROUP BY 
                 s.name, EXTRACT(HOUR FROM t.start_time)
             ORDER BY 
@@ -122,5 +200,5 @@ with DAG(
         location='US',
     )
 
-postgres_to_gcs >> gcs_to_bigquery >> check_table_existance >> [create_table, bike_trips_per_station_hour]
+postgres_to_gcs >> gcs_to_bigquery >> check_table_existance >> [skip_create_table, create_table] >> clean_data >> bike_trips_per_station_hour
 
